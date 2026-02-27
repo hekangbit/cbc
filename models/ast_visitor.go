5074b4a148c7578d89e9113c81c0a95223c45a6b
hekang <hekangbit@163.com>
2026-02-27 14:37:31 +0800

    refactor astvisitor and add todo list

--- models/ast_visitor.go
+++ models/ast_visitor.go
@@ -1,27 +1,42 @@
 package models
 
+// TODO：
 type IASTVisitor interface {
+	// Statements
 	VisitBlock(*ASTBlockNode) interface{}
-	VisitBinaryOp(*ASTBinaryOpNode) interface{}
-	VisitReturnNode(*ASTReturnNode) interface{}
 	VisitExprStmtNode(*ASTExprStmtNode) interface{}
+	// VisitIfStmtNode
+	// VisitSwitchNode
+	// VisitCaseNode
+	// VisitWhileNode
+	// VisitDoWhileNode
+	// VisitForNode
+	// VisitBreakNode
+	// VisitContinueNode
+	// VisitGotoNode
+	// VisitLabelNode
+	VisitReturnNode(*ASTReturnNode) interface{}
+
+	// Expressions
 	VisitAssignNode(*ASTAssignNode) interface{}
 	VisitOpAssignNode(*ASTOpAssignNode) interface{}
 	VisitCondExprNode(*ASTCondExprNode) interface{}
 	VisitLogicalOrNode(*ASTLogicalOrNode) interface{}
 	VisitLogicalAndNode(*ASTLogicalAndNode) interface{}
+	VisitBinaryOp(*ASTBinaryOpNode) interface{}
 	VisitUnaryOpNode(*ASTUnaryOpNode) interface{}
 	VisitPrefixOpNode(*ASTPrefixOpNode) interface{}
-	VisitDereferenceNode(*ASTDereferenceNode) interface{}
-	VisitAddressNode(*ASTAddressNode) interface{}
-	VisitSizeofTypeNode(*ASTSizeofTypeNode) interface{}
-	VisitSizeofExprNode(*ASTSizeofExprNode) interface{}
 	VisitSuffixOpNode(*ASTSuffixOpNode) interface{}
 	VisitArrayIdxRefNode(*ASTArrayIdxRefNode) interface{}
-	VisitFunctionCallNode(*ASTFunctionCallNode) interface{}
 	VisitMemberNodeNode(*ASTMemberNode) interface{}
 	VisitPtrMemberNode(*ASTPtrMemberNode) interface{}
-	VisitIntegerLiteralNode(*ASTIntegerLiteralNode) interface{}
+	VisitFunctionCallNode(*ASTFunctionCallNode) interface{}
+	VisitDereferenceNode(*ASTDereferenceNode) interface{}
+	VisitAddressNode(*ASTAddressNode) interface{}
+	// VisitCastNode
+	VisitSizeofExprNode(*ASTSizeofExprNode) interface{}
+	VisitSizeofTypeNode(*ASTSizeofTypeNode) interface{}
 	VisitVariableNode(*ASTVariableNode) interface{}
+	VisitIntegerLiteralNode(*ASTIntegerLiteralNode) interface{}
 	VisitStringLiteralNode(*ASTStringLiteralNode) interface{}
 }
